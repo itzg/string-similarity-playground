4e90de7f4e6255ebb698026c44169a5887579408
Geoff Bourne <itzgeoff@gmail.com>
2021-06-19 22:23:53 -0500

    Prettier tags rendering

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ function Input({value, setValue}) {
     )
 }
 
+function Tokens({tokens}) {
+    return tokens ? (
+        <div className="Tokens">
+            {
+                tokens.map(token => <span className="Token">{token}</span>)
+            }
+        </div>
+    ) : <></>;
+}
+
 function App() {
     const [left, setLeft] = useState("apple");
     const [leftTokens, setLeftTokens] = useState([]);
-    const [right, setRight] = useState("pineapple");
+    const [right, setRight] = useState("happen");
     const [rightTokens, setRightTokens] = useState([]);
     const [shingleSize, setShingleSize] = useState(3);
     const [result, setResult] = useState({});
@@ -32,16 +42,18 @@ function App() {
 
     return (
         <div className="App">
+            <h1>Sørensen–Dice coefficient string similarity playground</h1>
+            <a href="https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient">More information about this algorithm</a>
             <div>
                 <h2>Inputs</h2>
                 <div className="Inputs">
                     <div className="InputGroup">
                         <Input value={left} setValue={setLeft}/>
-                        {JSON.stringify(leftTokens)}
+                        <Tokens tokens={leftTokens}/>
                     </div>
                     <div className="InputGroup">
                         <Input value={right} setValue={setRight}/>
-                        {JSON.stringify(rightTokens)}
+                        <Tokens tokens={rightTokens}/>
                     </div>
                 </div>
             </div>
@@ -59,7 +71,7 @@ function App() {
                 <div className="Output">
                     <div>
                         <h3>Intersection</h3>
-                        {JSON.stringify(result.intersection)}
+                        <Tokens tokens={result.intersection}/>
                     </div>
                     <div><h3>Score</h3>
                         {result.score}
